716156464ec7f14b0bb10f419cf7aeabdba4ec16
JericHunter <jeric.hunterr@gmail.com>
2019-08-31 12:02:33 -0700

    story prints correctly

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,11 @@
+adj = input("Enter a adjective ")
+pronoun = input("Enter a pronoun ")
+verb = input("Enter a verb ")
+noun = input("Enter a noun ")
 
 
-print("I woke up really" + adjective + "into the night")
-print("I  could barely see" + pronoun + "because the" + verb "was out")
-print(noun + "was the only thing on my mind as I began" + verb + "my way to the" + noun)
-print("I" + verb + "very slowly trying to be" + adjective)
-print("but mother woke me up and told me to" + verb + "back to bed")
+print("I woke up really " + adj + " into the night")
+print("I could barely see " + pronoun + " because the " + verb + " was out")
+print(noun + " was the only thing on my mind as I began " + verb + " my way to the " + noun)
+print("I " + verb + " very slowly trying to be " + adj)
+print("but mother woke me up and told me to " + verb + " back to bed")
