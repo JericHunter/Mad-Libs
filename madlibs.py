ff530fed66811fc96854245bf115635838ac766f
JericHunter <jeric.hunterr@gmail.com>
2019-08-31 11:51:44 -0700

    mapped out story

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+
+
+print("I woke up really" + adjective + "into the night")
+print("I  could barely see" + pronoun + "because the" + verb "was out")
+print(noun + "was the only thing on my mind as I began" + verb + "my way to the" + noun)
+print("I" + verb + "very slowly trying to be" + adjective)
+print("but mother woke me up and told me to" + verb + "back to bed")
