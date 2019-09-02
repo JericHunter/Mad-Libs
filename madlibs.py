dd290309195171a37e7020a09b969660db1bdff4
JericHunter <jeric.hunterr@gmail.com>
2019-09-02 09:38:54 -0700

    sixth commit

--- madlibs.py
+++ madlibs.py
@@ -1,10 +1,10 @@
+
+
 print("Enter 2 adjectives ")
-# adj1 = input()
-# adj2 = input()
 adj = [input(),input()]
 
 print("Enter a pronoun ")
-pronoun = [input()]
+pronoun = input()
 
 print("Enter 4 verbs ")
 verb = [input(),input(),input(),input()]
@@ -12,8 +12,12 @@ verb = [input(),input(),input(),input()]
 print("Enter 2 nouns ")
 noun = [input(),input()]
 
-print("I woke up really " + adj[0] + " into the night")
-print("I could barely see " + pronoun[0] + " because the " + verb[0] + " was out")
-print(noun[1] + " was the only thing on my mind as I began " + verb[1] + " my way to the " + noun[0])
-print("I " + verb[2] + " very slowly trying to be " + adj[1])
-print("but mother woke me up and told me to " + verb[3] + " back to bed")
+def libs():
+    print("I woke up really " + adj[0] + " into the night")
+    print("I could barely see " + pronoun + " because the " + verb[0] + " was out")
+    print(noun[1] + " was the only thing on my mind as I began " + verb[1] + " my way to the " + noun[0])
+    print("I " + verb[2] + " very slowly trying to be " + adj[1])
+    print("but mother woke me up and told me to " + verb[3] + " back to bed")
+
+
+libs()
